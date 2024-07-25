a3af4d214bd06558327e0b634f1f0a73ed70e2bd
AnonymousWoman <anonymouswoman@example.org>
2024-07-25 22:31:20 +0800

    Add led off code.

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main()
     if (console_in_number == 1) {
         // TODO: write the cli_led reg 0 with all 1.
     } else if(console_in_number == 0) {
-        // TODO: write the cli_led reg 0 with all 0.
+        CLI_LED_mWriteReg(XPAR_CLI_LED_0_BASEADDR, CLI_LED_S00_AXI_SLV_REG0_OFFSET, 0x00000000);
     }
 
     cleanup_platform();
